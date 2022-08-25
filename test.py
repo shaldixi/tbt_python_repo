44a8aa225c41715b518e48e2225de47d96840ead
shaldixi <65905068+shaldixi@users.noreply.github.com>
2022-08-25 06:34:09 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
 print("Hey How are you?")
+with open("text.txt","w") as file:
+    file.write("I am learning Python!\n")
+    file.write("I am really enjoying it!\n")
+    file.write("And I want to add more lines to say how much I like it")
